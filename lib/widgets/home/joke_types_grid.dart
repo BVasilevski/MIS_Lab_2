f3bbdc304ed617a5e74bca58c3f7588745cb7d70
= <bojan.vasilevski@students.finki.ukim.mk>
2024-12-12 19:04:04 +0100

    Fixed text in box issue

--- lib/widgets/home/joke_types_grid.dart
+++ lib/widgets/home/joke_types_grid.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:lab_2/widgets/home/joke_type_card.dart';
+import 'joke_type_card.dart';
 
-
-class JokeTypesGrid extends StatefulWidget {
+class JokeTypesGrid extends StatelessWidget {
   final List<String> jokeTypes;
 
   const JokeTypesGrid({super.key, required this.jokeTypes});
 
   @override
-  State<JokeTypesGrid> createState() => _JokeTypesGridState();
-}
-
-class _JokeTypesGridState extends State<JokeTypesGrid> {
-  @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(top: 50.0),
-        child: GridView.count(
-          padding: const EdgeInsets.all(8),
-          crossAxisCount: 4,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-          childAspectRatio: 1,
-          shrinkWrap: true,
-          children:
-              widget.jokeTypes.map((type) => JokeTypeCard(type: type)).toList(),
-        ));
+      padding: const EdgeInsets.only(top: 50.0),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          int crossAxisCount = (constraints.maxWidth / 150).floor();
+          return GridView.builder(
+            padding: const EdgeInsets.all(8),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: crossAxisCount > 0
+                  ? crossAxisCount
+                  : 1, // Ensure at least 1 column
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 1, // Square grid items
+            ),
+            itemCount: jokeTypes.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return JokeTypeCard(type: jokeTypes[index]);
+            },
+          );
+        },
+      ),
+    );
   }
 }
